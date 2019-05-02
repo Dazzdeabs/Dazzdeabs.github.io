0547b36c4bcc56b076287980101b1164f33711ec
Darren <20453663@sun.ac.za>
2019-05-02 08:46:31 +0200

    Updates

---
+++ precache-manifest.94b73e22d93c0c9ab42edb0e6d4c04c1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "97927ff4d38e6841c298ca38c6db4e07",
+    "url": "/repo/static/media/mountain.97927ff4.png"
+  },
+  {
+    "revision": "569b8f1f2f502ba3c43c782801d71edd",
+    "url": "/repo/static/media/logo.569b8f1f.jpg"
+  },
+  {
+    "revision": "99950a0df3e856f7907b3bae86d775f6",
+    "url": "/repo/static/media/info.99950a0d.png"
+  },
+  {
+    "revision": "6d911ade77711aa4b727cfdae75a26e8",
+    "url": "/repo/static/media/contact.6d911ade.jpg"
+  },
+  {
+    "revision": "3da021c15adfa4073df2",
+    "url": "/repo/static/js/runtime~main.3da021c1.js"
+  },
+  {
+    "revision": "8aec78b8605862362fd3",
+    "url": "/repo/static/js/main.8aec78b8.chunk.js"
+  },
+  {
+    "revision": "62050606f093638b8473",
+    "url": "/repo/static/js/1.62050606.chunk.js"
+  },
+  {
+    "revision": "8aec78b8605862362fd3",
+    "url": "/repo/static/css/main.88562557.chunk.css"
+  },
+  {
+    "revision": "ef9f89fe26c33352008b97548b1ad8be",
+    "url": "/repo/index.html"
+  }
+];
